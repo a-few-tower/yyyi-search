bbfbb32bce40bee65d682764f78039a3608b951c
a-few-tower <40914913+a-few-tower@users.noreply.github.com>
2019-05-07 17:27:16 +0900

    adopt collection objects.

--- public/js/yyyi/search.js
+++ public/js/yyyi/search.js
@@ -3,8 +3,42 @@
  */
 YYYI.search = (function(){
   var db;  // データベース
-  var yusha;  // yushaテーブル
-  var skill;  // skillテーブル
+  var yusha = {
+    collection: null,  // yushaテーブル
+    fields: {
+      id: 'a',
+      code: 'b',
+      title: 'c',
+      name: 'd',
+      rare: 'e',
+      color: 'f',
+      range: 'g',
+      leader_skill: 'h',
+      leader_skill_desc: 'i',
+      arts: 'j',
+      arts_desc: 'k',
+      ability: 'l',
+      ability_desc: 'm',
+    }
+  };
+  var skill = {
+    collection: null,  // skillテーブル
+    fields: {
+      id: 'a',
+      code: 'b',
+      skill_type: 'c',
+      when: 'd',
+      how_long: 'e',
+      whom: 'f',
+      friend_enemy: 'g',
+      what: 'h',
+      advantage: 'i',
+      value: 'j',
+      unit: 'k',
+      good_bad: 'l',
+    }
+  };
+
   /**
    * データベース初期化。
    */
@@ -12,9 +46,9 @@ YYYI.search = (function(){
     // データベース作成
     db = new loki("yyyi.db");
     // yushaテーブル初期化
-    yusha = db.addCollection('yusha');
+    yusha.collection = db.addCollection('yusha');
     // skillテーブル初期化
-    skill = db.addCollection('skill');
+    skill.collection = db.addCollection('skill');
 
     _initByDataJs();
   }
@@ -23,10 +57,10 @@ YYYI.search = (function(){
    */
   function _initByDataJs() {
     for (var y of YYYI.data.loadY()) {
-      yusha.insert(y);
+      yusha.collection.insert(y);
     }
     for (var s of YYYI.data.loadS()) {
-      skill.insert(s);
+      skill.collection.insert(s);
     }
   }
   /**
@@ -46,9 +80,9 @@ YYYI.search = (function(){
 
       // スキル検索の結果が1件以上あればコードを絞り込み条件に追加
       } else {
-        skills.forEach((skill) => {
-          if (code.indexOf(skill.b) === -1) {
-            code.push(skill.b);
+        skills.forEach((doc) => {
+          if (code.indexOf(doc[skill.fields.code]) === -1) {
+            code.push(doc[skill.fields.code]);
           }
         });
         params["code"] = code;
@@ -62,27 +96,28 @@ YYYI.search = (function(){
    * @return {Array|bool} 条件に合ったskillの配列
    */
   function _findSkill(params) {
-    var $and = [];
+    var conditions = [];
+
     var skillType = params["skillType"];
     if (skillType.length > 0 && skillType[0] != "0") {
-      $and.push({'c': {'$in': _toNumArr(skillType)}});
+      conditions.push({[skill.fields.skill_type]: {'$in': _toNumArr(skillType)}});
     }
     var skillTrigger = params["skillTrigger"];
     if (skillTrigger.length > 0 && skillTrigger[0] != "0") {
-      $and.push({'d': {'$in': _toNumArr(skillTrigger)}});
+      conditions.push({[skill.fields.when]: {'$in': _toNumArr(skillTrigger)}});
     }
     var skillTarget = params["skillTarget"];
     if (skillTarget.length > 0 && skillTarget[0] != "0") {
-      $and.push({'f': {'$in': _toNumArr(skillTarget)}});
+      conditions.push({[skill.fields.whom]: {'$in': _toNumArr(skillTarget)}});
     }
     var skillStatus = params["skillStatus"];
     if (skillStatus.length > 0 && skillStatus[0] != "0") {
-      $and.push({'h': {'$in': _toNumArr(skillStatus)}});
+      conditions.push({[skill.fields.what]: {'$in': _toNumArr(skillStatus)}});
     }
-    if ($and.length == 0) {
+    if (conditions.length == 0) {
       return false;
     } else {
-      return skill.find({'$and': $and});
+      return skill.collection.find({'$and': conditions});
     }
   }
   /**
@@ -91,37 +126,41 @@ YYYI.search = (function(){
    * @return {Array} 条件に合ったyushaの配列
    */
   function _findYusha(params) {
-    var $and = [];
+    var conditions = [];
+
     var code = params["code"];
     if (code.length > 0) {
-      $and.push({'b': {'$in': code}});
+      conditions.push({[yusha.fields.code]: {'$in': code}});
     }
     var name = params["name"];
     if (name.length > 0 && name[0] != "0") {
-      $and.push({'d': {'$in': _toNumArr(name)}});
+      conditions.push({[yusha.fields.name]: {'$in': _toNumArr(name)}});
     }
     var rare = params["rare"];
     if (rare.length > 0 && rare[0] != "0") {
-      $and.push({'e': {'$in': _toNumArr(rare)}});
+      conditions.push({[yusha.fields.rare]: {'$in': _toNumArr(rare)}});
     }
     var color = params["color"];
     if (color.length > 0 && color[0] != "0") {
-      $and.push({'f': {'$in': _toNumArr(color)}});
+      conditions.push({[yusha.fields.color]: {'$in': _toNumArr(color)}});
     }
     var range = params["range"];
     if (range.length > 0 && range[0] != "0") {
-      $and.push({'g': {'$in': _toNumArr(range)}});
+      conditions.push({[yusha.fields.range]: {'$in': _toNumArr(range)}});
     }
-    return yusha.chain()
-      .find({'$and': $and})
-      // レアリティ降順、属性昇順、キャラクター昇順、コード昇順
-      .compoundsort([['e', true],['f', false],['d', false],['b', false]])
+    return yusha.collection.chain()
+      .find({'$and': conditions})
+      .compoundsort([
+        [yusha.fields.rare, true],  // レアリティ降順
+        [yusha.fields.color, false],  // 属性昇順
+        [yusha.fields.name, false],  // キャラクター昇順
+        [yusha.fields.code, false]])  // コード昇順
       .data();
   }
   /**
    * 文字列配列を数値配列に変換して返す。
    * @param {Array} arr 文字列配列
-   * @return {Array} 数値配列 
+   * @return {Array} 数値配列
    */
   function _toNumArr(arr) {
     var ret = [];
